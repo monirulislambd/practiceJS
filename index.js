012643591fbf7b0e36c6ae87911ccdaf511b5788
monirulislambd <124524891+monirulislambd@users.noreply.github.com>
2023-02-06 01:13:35 +0600

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ import './style.css';
 
 console.log("Hello Everyone!");
 
+const syncWithCallBach = () => {
+  for (let index = 0; index<10; index++){
+
+    console.log("Looping:\t", index, "\t", new Date());
+
+  }
+}
+
+syncWithCallBach();
+console.log("All Done");
+
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
